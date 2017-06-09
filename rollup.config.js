36386e4ca3bfdcec541130d5499ef0e7482975e5
William Swanson <swansontec@gmail.com>
2017-06-08 18:04:38 -0700

    Display file sizes duing build

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,27 @@
 import buble from 'rollup-plugin-buble'
+import uglify from 'rollup-plugin-uglify'
+import filesize from 'rollup-plugin-filesize'
 const packageJson = require('./package.json')
 
-export default {
-  entry: 'src/index.js',
-  plugins: [
-    buble({
-      transforms: {
-        dangerousForOf: true
-      }
-    })
-  ],
-  targets: [
-    {
-      dest: packageJson['main'],
-      format: 'cjs',
-      sourceMap: true
-    }
-  ]
+const bubleOpts = {
+  transforms: {
+    dangerousForOf: true
+  }
 }
+
+export default [
+  {
+    dest: packageJson['main'],
+    entry: 'src/index.js',
+    format: 'cjs',
+    plugins: [buble(bubleOpts)],
+    sourceMap: true
+  },
+  {
+    dest: 'lib/index.min.js',
+    entry: 'src/index.js',
+    format: 'iife',
+    moduleName: 'rfc4648',
+    plugins: [buble(bubleOpts), uglify(), filesize()]
+  }
+]
