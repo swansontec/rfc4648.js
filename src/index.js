d38d73ae524ff47d3fbd06a74dab7cfe009134cb
William Swanson <swansontec@gmail.com>
2019-04-05 16:12:04 -0700

    Flatten the library export structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,88 @@
-import * as base16 from './base16.js'
-import * as base32 from './base32.js'
-import * as base32hex from './base32hex.js'
-import * as base64 from './base64.js'
-import * as base64url from './base64url.js'
-import * as codec from './codec.js'
+import { parse, stringify } from './codec.js'
 
-export { base16, base32, base32hex, base64, base64url, codec }
+const base16Encoding = {
+  chars: '0123456789ABCDEF',
+  bits: 4
+}
+
+export const base16 = {
+  parse(string, opts) {
+    return parse(string.toUpperCase(), base16Encoding, opts)
+  },
+
+  stringify(data, opts) {
+    return stringify(data, base16Encoding, opts)
+  }
+}
+
+const base32Encoding = {
+  chars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567',
+  bits: 5
+}
+
+export const base32 = {
+  parse(string, opts = {}) {
+    return parse(
+      opts.loose
+        ? string
+            .toUpperCase()
+            .replace(/0/g, 'O')
+            .replace(/1/g, 'L')
+            .replace(/8/g, 'B')
+        : string,
+      base32Encoding,
+      opts
+    )
+  },
+
+  stringify(data, opts) {
+    return stringify(data, base32Encoding, opts)
+  }
+}
+
+const base32HexEncoding = {
+  chars: '0123456789ABCDEFGHIJKLMNOPQRSTUV',
+  bits: 5
+}
+
+export const base32hex = {
+  parse(string, opts) {
+    return parse(string, base32HexEncoding, opts)
+  },
+
+  stringify(data, opts) {
+    return stringify(data, base32HexEncoding, opts)
+  }
+}
+
+const base64Encoding = {
+  chars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/',
+  bits: 6
+}
+
+export const base64 = {
+  parse(string, opts) {
+    return parse(string, base64Encoding, opts)
+  },
+
+  stringify(data, opts) {
+    return stringify(data, base64Encoding, opts)
+  }
+}
+
+const base64UrlEncoding = {
+  chars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_',
+  bits: 6
+}
+
+export const base64url = {
+  parse(string, opts) {
+    return parse(string, base64UrlEncoding, opts)
+  },
+
+  stringify(data, opts) {
+    return stringify(data, base64UrlEncoding, opts)
+  }
+}
+
+export const codec = { parse, stringify }
