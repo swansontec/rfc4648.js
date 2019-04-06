55cbc06e2226dd30846eecd3d004e1dc4ea746ab
William Swanson <swansontec@gmail.com>
2019-04-06 00:39:24 -0700

    Replace buble with babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,34 @@
-import buble from 'rollup-plugin-buble'
+import babel from 'rollup-plugin-babel'
 import filesize from 'rollup-plugin-filesize'
 import { uglify } from 'rollup-plugin-uglify'
 
 import packageJson from './package.json'
 
-const bubleOpts = {
-  transforms: {
-    dangerousForOf: true
-  }
+const babelOpts = {
+  babelrc: false,
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        exclude: ['transform-regenerator'],
+        loose: true
+      }
+    ]
+  ]
 }
 
 export default [
   {
     input: 'src/index.js',
-    output: {
-      file: packageJson['main'],
-      format: 'cjs',
-      sourceMap: true
-    },
-    plugins: [buble(bubleOpts)]
+    output: [
+      { file: packageJson.module, format: 'esm', sourceMap: true },
+      { file: packageJson.main, format: 'cjs', sourceMap: true }
+    ],
+    plugins: [babel(babelOpts)]
   },
   {
     input: 'src/index.js',
-    output: {
-      file: 'lib/index.min.js',
-      format: 'iife',
-      name: 'rfc4648'
-    },
-    plugins: [buble(bubleOpts), uglify(), filesize()]
+    output: { file: 'lib/index.min.js', format: 'iife', name: 'rfc4648' },
+    plugins: [babel(babelOpts), uglify(), filesize()]
   }
 ]
