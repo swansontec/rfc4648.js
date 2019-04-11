ecb1244d6d204ac14ae88375e598508b39ca15c9
William Swanson <swansontec@gmail.com>
2019-04-10 17:10:07 -0700

    Upgrade the test suite
    Mocha makes it possible to generate tests on the fly, so use that to give clearer messages.

--- test/tests.js
+++ test/tests.js
@@ -1,203 +1,195 @@
 /* global describe, it */
-const assert = require('assert')
-const rfc4648 = require('../lib/index.cjs.js')
-
-const specVectors = [
-  ['base64', '', ''],
-  ['base64', 'f', 'Zg=='],
-  ['base64', 'fo', 'Zm8='],
-  ['base64', 'foo', 'Zm9v'],
-  ['base64', 'foob', 'Zm9vYg=='],
-  ['base64', 'fooba', 'Zm9vYmE='],
-  ['base64', 'foobar', 'Zm9vYmFy'],
-  ['base32', '', ''],
-  ['base32', 'f', 'MY======'],
-  ['base32', 'fo', 'MZXQ===='],
-  ['base32', 'foo', 'MZXW6==='],
-  ['base32', 'foob', 'MZXW6YQ='],
-  ['base32', 'fooba', 'MZXW6YTB'],
-  ['base32', 'foobar', 'MZXW6YTBOI======'],
-  ['base32hex', '', ''],
-  ['base32hex', 'f', 'CO======'],
-  ['base32hex', 'fo', 'CPNG===='],
-  ['base32hex', 'foo', 'CPNMU==='],
-  ['base32hex', 'foob', 'CPNMUOG='],
-  ['base32hex', 'fooba', 'CPNMUOJ1'],
-  ['base32hex', 'foobar', 'CPNMUOJ1E8======'],
-  ['base16', '', ''],
-  ['base16', 'f', '66'],
-  ['base16', 'fo', '666F'],
-  ['base16', 'foo', '666F6F'],
-  ['base16', 'foob', '666F6F62'],
-  ['base16', 'fooba', '666F6F6261'],
-  ['base16', 'foobar', '666F6F626172']
-]
-
-const dataVectors = [
-  [
-    'base16',
-    [0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef],
-    '0123456789ABCDEF'
-  ],
-  [
-    'base32',
-    [0x00, 0x44, 0x32, 0x14, 0xc7, 0x42, 0x54, 0xb6, 0x35, 0xcf],
-    'ABCDEFGHIJKLMNOP'
-  ],
-  [
-    'base32',
-    [0x84, 0x65, 0x3a, 0x56, 0xd7, 0xc6, 0x75, 0xbe, 0x77, 0xdf],
-    'QRSTUVWXYZ234567'
-  ],
-  [
-    'base32hex',
-    [0x00, 0x44, 0x32, 0x14, 0xc7, 0x42, 0x54, 0xb6, 0x35, 0xcf],
-    '0123456789ABCDEF'
-  ],
-  [
-    'base32hex',
-    [0x84, 0x65, 0x3a, 0x56, 0xd7, 0xc6, 0x75, 0xbe, 0x77, 0xdf],
-    'GHIJKLMNOPQRSTUV'
-  ],
-  [
-    'base64',
-    [0x00, 0x10, 0x83, 0x10, 0x51, 0x87, 0x20, 0x92, 0x8b, 0x30, 0xd3, 0x8f],
-    'ABCDEFGHIJKLMNOP'
-  ],
-  [
-    'base64',
-    [0x41, 0x14, 0x93, 0x51, 0x55, 0x97, 0x61, 0x96, 0x9b, 0x71, 0xd7, 0x9f],
-    'QRSTUVWXYZabcdef'
-  ],
-  [
-    'base64',
-    [0x82, 0x18, 0xa3, 0x92, 0x59, 0xa7, 0xa2, 0x9a, 0xab, 0xb2, 0xdb, 0xaf],
-    'ghijklmnopqrstuv'
-  ],
-  [
-    'base64',
-    [0xc3, 0x1c, 0xb3, 0xd3, 0x5d, 0xb7, 0xe3, 0x9e, 0xbb, 0xf3, 0xdf, 0xbf],
-    'wxyz0123456789+/'
-  ],
-  ['base64', [0xfb, 0xff], '+/8='],
-  ['base64url', [0xfb, 0xff], '-_8=']
-]
-
-const failVectors = [
-  ['base16', '0'],
-  ['base16', '0='],
-  ['base16', '00=', true],
-  // Illegal characters:
-  ['base32', 'A1======'],
-  ['base32', 'A9======'],
-  ['base32', 'Aa======', true],
-  ['base32', 'He1l0===', true],
-  // Non-byte ending:
-  ['base32', 'A======='],
-  ['base32', 'A7======'],
-  ['base32', 'AAA====='],
-  ['base32', 'AAAAAA=='],
-  // Padding issues:
-  ['base32', 'AA', true],
-  ['base32', 'AA==', true],
-  ['base32', 'AAAA', true],
-  ['base32', 'AAAAA', true],
-  ['base32', 'AAAAAAA', true],
-  ['base32', 'AAAAAAAA========', true],
-  // Non-byte ending:
-  ['base64', 'A==='],
-  ['base64', '+/+='],
-  ['base64', 'AAAAA'],
-  // Padding issues:
-  ['base64', 'AA=', true],
-  ['base64', 'AAAA====', true]
-]
+const { expect } = require('chai')
+const {
+  base16,
+  base32,
+  base32hex,
+  base64,
+  base64url
+} = require('../lib/index.cjs.js')
 
 function parseAscii (string) {
-  const out = []
+  const out = new Uint8Array(string.length)
   for (let i = 0; i < string.length; ++i) {
     out[i] = string.charCodeAt(i)
   }
   return out
 }
 
-describe('rfc4648', function () {
-  it('should match the official test vectors', function () {
-    specVectors.forEach(vector => {
-      const [codec, input, string] = vector
-      const data = parseAscii(input)
-
-      assert.equal(rfc4648[codec].stringify(data), string)
-      assert.deepEqual(rfc4648[codec].parse(string), data)
+function generateTests (codec, vectors) {
+  for (const [data, text] of vectors) {
+    it(`round-trips "${text}"`, function () {
+      const expected =
+        typeof data === 'string' ? parseAscii(data) : Uint8Array.from(data)
+      expect(codec.stringify(expected)).equals(text)
+      expect(codec.parse(text)).deep.equals(expected)
     })
-  })
+  }
+}
 
-  it('should match our extra test vectors', function () {
-    dataVectors.forEach(vector => {
-      const [codec, data, string] = vector
-
-      assert.equal(rfc4648[codec].stringify(data), string)
-      assert.deepEqual(rfc4648[codec].parse(string), data)
-    })
-  })
-
-  it('should fail on invalid input', function () {
-    failVectors.forEach(vector => {
-      const [codec, string, loose] = vector
-
-      // Strict and loose versions:
-      function f1 () {
-        rfc4648[codec].parse(string)
-      }
-      function f2 () {
-        rfc4648[codec].parse(string, { loose: true })
-      }
-
-      // Verify exceptions:
-      assert.throws(f1, string)
-      if (loose) {
-        assert.doesNotThrow(f2, string)
-      } else {
-        assert.throws(f2, string)
-      }
-    })
-  })
-
-  it('Should support non-array return types', function () {
-    const out = rfc4648.base16.parse('f00d', { out: Uint8Array })
-    assert(out instanceof Uint8Array)
-  })
-})
+function generateErrorTests (codec, vectors) {
+  for (const [text, error, loose] of vectors) {
+    if (loose == null || typeof loose === 'string') {
+      it(`rejects "${text}"`, function () {
+        expect(() => codec.parse(text)).throws(error)
+        expect(() => codec.parse(text, { loose: true })).throws(loose || error)
+      })
+    } else {
+      it(`loosely parses "${text}"`, function () {
+        const expected = Uint8Array.from(loose)
+        expect(() => codec.parse(text)).throws(error)
+        expect(codec.parse(text, { loose: true })).deep.equals(expected)
+      })
+    }
+  }
+}
 
 describe('base16', function () {
-  it('should decode lowercase characters', function () {
-    assert.deepEqual(rfc4648.base16.parse('abcdef'), [0xab, 0xcd, 0xef])
+  generateTests(base16, [
+    ['', ''],
+    ['f', '66'],
+    ['fo', '666F'],
+    ['foo', '666F6F'],
+    ['foob', '666F6F62'],
+    ['fooba', '666F6F6261'],
+    ['foobar', '666F6F626172'],
+    // Full alphabet:
+    [[0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef], '0123456789ABCDEF']
+  ])
+
+  generateErrorTests(base16, [
+    ['0', 'Invalid padding', 'Unexpected end of data'],
+    ['0=', 'Unexpected end of data'],
+    ['00=', 'Invalid padding', [0x00]]
+  ])
+
+  it('decodes lowercase characters', function () {
+    const expected = Uint8Array.from([0xab, 0xcd, 0xef])
+    expect(base16.parse('abcdef')).deep.equals(expected)
   })
 
-  it('should work return Uint8Array by default', function () {
-    const out = rfc4648.base16.parse('abcdef')
-    assert(out instanceof Uint8Array)
-    assert.deepEqual(out, [0xab, 0xcd, 0xef])
-  })
-
-  it('should work with Node.js Buffers', function () {
-    const out = rfc4648.base16.parse('abcdef', { out: Buffer.allocUnsafe })
-    assert(out instanceof Buffer)
-    assert.deepEqual(out, [0xab, 0xcd, 0xef])
+  it('works with plain arrays', function () {
+    const expected = [0xab, 0xcd, 0xef]
+    expect(base16.parse('abcdef', { out: Array })).deep.equals(expected)
   })
 })
 
 describe('base32', function () {
-  it('should fix common typos in loose mode', function () {
-    assert.deepEqual(
-      rfc4648.base32.parse('He1l0===', { loose: true }),
-      rfc4648.base32.parse('HELLO===')
+  generateTests(base32, [
+    // rfc4648:
+    ['', ''],
+    ['f', 'MY======'],
+    ['fo', 'MZXQ===='],
+    ['foo', 'MZXW6==='],
+    ['foob', 'MZXW6YQ='],
+    ['fooba', 'MZXW6YTB'],
+    ['foobar', 'MZXW6YTBOI======'],
+    // Full alphabet:
+    [
+      [0x00, 0x44, 0x32, 0x14, 0xc7, 0x42, 0x54, 0xb6, 0x35, 0xcf],
+      'ABCDEFGHIJKLMNOP'
+    ],
+    [
+      [0x84, 0x65, 0x3a, 0x56, 0xd7, 0xc6, 0x75, 0xbe, 0x77, 0xdf],
+      'QRSTUVWXYZ234567'
+    ]
+  ])
+
+  generateErrorTests(base32, [
+    // Illegal characters:
+    ['A1======', 'Invalid character 1', 'Unexpected end of data'],
+    ['A9======', 'Invalid character 9'],
+    ['Aa======', 'Invalid character a', [0]],
+    ['He1l0===', 'Invalid character e', [57, 22, 183]],
+    // Non-byte ending:
+    ['A=======', 'Unexpected end of data'],
+    ['A7======', 'Unexpected end of data'],
+    ['AAA=====', 'Unexpected end of data'],
+    ['AAAAAA==', 'Unexpected end of data'],
+    // Padding issues:
+    ['AA', 'Invalid padding', [0]],
+    ['AA==', 'Invalid padding', [0]],
+    ['AAAA', 'Invalid padding', [0, 0]],
+    ['AAAAA', 'Invalid padding', [0, 0, 0]],
+    ['AAAAAAA', 'Invalid padding', [0, 0, 0, 0]],
+    ['AAAAAAAA========', 'Invalid padding', [0, 0, 0, 0, 0]]
+  ])
+
+  it('Fixes common typos in loose mode', function () {
+    expect(base32.parse('He1l0===', { loose: true })).deep.equals(
+      base32.parse('HELLO===')
     )
   })
 })
 
+describe('base32hex', function () {
+  generateTests(base32hex, [
+    // rfc4648:
+    ['', ''],
+    ['f', 'CO======'],
+    ['fo', 'CPNG===='],
+    ['foo', 'CPNMU==='],
+    ['foob', 'CPNMUOG='],
+    ['fooba', 'CPNMUOJ1'],
+    ['foobar', 'CPNMUOJ1E8======'],
+    // Full alphabet:
+    [
+      [0x00, 0x44, 0x32, 0x14, 0xc7, 0x42, 0x54, 0xb6, 0x35, 0xcf],
+      '0123456789ABCDEF'
+    ],
+    [
+      [0x84, 0x65, 0x3a, 0x56, 0xd7, 0xc6, 0x75, 0xbe, 0x77, 0xdf],
+      'GHIJKLMNOPQRSTUV'
+    ]
+  ])
+})
+
+describe('base64', function () {
+  generateTests(base64, [
+    // rfc4648:
+    ['', ''],
+    ['f', 'Zg=='],
+    ['fo', 'Zm8='],
+    ['foo', 'Zm9v'],
+    ['foob', 'Zm9vYg=='],
+    ['fooba', 'Zm9vYmE='],
+    ['foobar', 'Zm9vYmFy'],
+    // Full alphabet:
+    [
+      [0x00, 0x10, 0x83, 0x10, 0x51, 0x87, 0x20, 0x92, 0x8b, 0x30, 0xd3, 0x8f],
+      'ABCDEFGHIJKLMNOP'
+    ],
+    [
+      [0x41, 0x14, 0x93, 0x51, 0x55, 0x97, 0x61, 0x96, 0x9b, 0x71, 0xd7, 0x9f],
+      'QRSTUVWXYZabcdef'
+    ],
+    [
+      [0x82, 0x18, 0xa3, 0x92, 0x59, 0xa7, 0xa2, 0x9a, 0xab, 0xb2, 0xdb, 0xaf],
+      'ghijklmnopqrstuv'
+    ],
+    [
+      [0xc3, 0x1c, 0xb3, 0xd3, 0x5d, 0xb7, 0xe3, 0x9e, 0xbb, 0xf3, 0xdf, 0xbf],
+      'wxyz0123456789+/'
+    ],
+    // base64url characters:
+    [[0xfb, 0xff], '+/8=']
+  ])
+
+  generateErrorTests(base64, [
+    // Non-byte ending:
+    ['A===', 'Unexpected end of data'],
+    ['+/+=', 'Unexpected end of data'],
+    ['AAAAA', 'Invalid padding', 'Unexpected end of data'],
+    // Padding issues:
+    ['AA=', 'Invalid padding', [0]],
+    ['AAAA====', 'Invalid padding', [0, 0, 0]]
+  ])
+})
+
 describe('base64url', function () {
+  generateTests(base64url, [[[0xfb, 0xff], '-_8=']])
+
   it('should work without padding', function () {
-    assert.deepEqual(rfc4648.base64url.stringify([0x00], { pad: false }), 'AA')
+    expect(base64url.stringify([0x00], { pad: false })).equals('AA')
   })
 })
